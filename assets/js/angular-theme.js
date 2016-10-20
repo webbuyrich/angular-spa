da3fd25facd72f2da7942a1e849178e1a8b8e0d6
Richard Peterson <webbuyrich@gmail.com>
2016-10-20 12:05:28 -0500

    created route to display posts in json format

--- assets/js/angular-theme.js
+++ assets/js/angular-theme.js
@@ -1,18 +1,27 @@
-// adding new ng-App model
 var wpApp = new angular.module( 'wpAngularTheme', ['ui.router', 'ngResource'] );
 
-
-wpApp.factory( 'Posts', function( $resource ){
+wpApp.factory( 'Posts', function( $resource ) {
 	return $resource( appInfo.api_url + 'posts/:ID', {
 		ID: '@id'
 	})
 });
 
-wpApp.controller('ListController', ['$scope', 'Posts', function($scope, Posts){
+wpApp.controller( 'ListController', ['$scope', 'Posts', function( $scope, Posts ) {
 	console.log('ListController');
-	$scope.page_title = 'Blog Listing';
-	Posts.query(function(res){
+	$scope.page_title = 'Blog Listing Page';
+
+	Posts.query(function( res ) {
 		$scope.posts = res;
 	});
+	
 }]);
 
+wpApp.config( function( $stateProvider, $urlRouterProvider){
+	$urlRouterProvider.otherwise('/');
+	$stateProvider
+		.state( 'list', {
+			url: '/',
+			controller: 'ListController',
+			templateUrl: appInfo.template_directory + 'templates/list.html'
+		})
+})
--- instructions.php
+++ instructions.php
@@ -15,26 +15,32 @@
         		<p>Install this plugin from within the admin area</p>
         	</div>
             <div class="panel">
-                <h1>2. Add new Angular Module</h1>
+                <h1>3. Add new Angular Module</h1>
                 <p>In the angular-theme.js, create an angular module as a variable, give the module a name,
                     and add the ui.router dependency.</p>
             </div>
             <div class="panel">
-                <h1>2. Add ngResource FACTORY</h1>
+                <h1>4. Add ngResource FACTORY</h1>
                 <p>add ngResource next to ui.router, add the ng-resource cdn script to functions.php 
                     change the version in the link, and add ('angular-core') array with syntax like above script.</p>
             </div>
             <div class="panel">
-                <h1>2. Add ListCtrl CONTROLLER</h1>
+                <h1>5. Add ListCtrl CONTROLLER</h1>
                 <p>Create a controller named 'ListController' and add Posts next to $scope before and after the function.
                     Add a scope page title and add Posts.query funciton that uses res as the input set $scope.posts equal 
                     to the res input from the function.</p>
             </div>
             <div class="panel">
-                <h1>2. Add Template</h1>
+                <h1>6. Add Template</h1>
                 <p>Create a new folder named templates. Add a file named list.html inside templates folder. Use h1 tag 
                     to display page title that was defined in the controller. use pre and code to view all posts code
                     and using {posts | json}</p>
+            </div>
+            <div class="panel">
+                <h1>7. Add Route</h1>
+                <p>Create a new wpApp.config function using $stateProvider and $urlRouterProvider. View the stateprovider
+                    syntax to set the name, url, controller, and templateUrl. Also add urlRouterProvider.otherwise and set it
+                    to '/' to go to the homepage if an error occurs</p>
             </div>
         </div>
         <script src="js/main.js"></script>
